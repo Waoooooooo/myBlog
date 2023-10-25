210c7f72d07bae9a2b8e661d6ff805d0e596be9d
Waoooooooo <1185202122@qq.com>
2023-10-25 20:25:37 +0800

    配置前端开发环境

--- api文档.md
+++ api文档.md
@@ -1,2 +1,39 @@
-api/
-++
+**统一响应格式**
+
+***
+{
+    "success": Boolean, // 响应结果(true/false)
+    "code": Integer, // 状态码(20000/20001/21001/21002...)
+    "message": String, // 返回消息
+    "data": HashMap // 数据
+}
+***
+
+
+没有返回数据时
+{
+    "success": Boolean,
+    "code": Integer,
+    "message": String,
+    "data": {}
+}
+
+
+
+返回数据中有多个数据列表
+{
+    "success": Boolean,
+    "code": Integer,
+    "message": String,
+    "data": {
+        "items": [ //列表
+            {
+                "id": 1,
+                "username": "AydenBryan",
+                "password": "123456"
+            },
+            ...
+        ]
+    }
+}
+
--- index.mjs
+++ index.mjs
@@ -1,43 +1,105 @@
 import express from "express";
-import {dbApi} from "./db.mjs";
+import { dbApi } from "./db.mjs";
 
 
 
 const app = express()
-//app是一个服务器实例
+//创建服务器实例
 const port = 10086
 
 //日志
-app.get('/*', (req, res,next) => {
+app.all('/*', (req, res, next) => {
   console.log(req.url)
+  console.log("请求体:",req.body)
   next()
 })
-
 
 //静态文件服务
 app.use(express.static('public'))
 
-
+//--------博客信息查询-----------------
 //通过id查询一篇博客  返回undefined 或者 一个对象
-app.get('/blog/:id', (req, res,next) => {
-  console.log("id的值",req.params.id)
-  console.log(dbApi.getBlogById(req.params.id))
-  res.send(dbApi.getBlogById(req.params.id))
+app.get('/blog/:id', (req, res, next) => {
+  const blog = dbApi.getBlogById(req.params.id)
+  req.data = blog
+  next()
 })
-
-
 //获取所有博客
-app.get('/blogs', (req, res,next) => {
-  res.send(dbApi.getBlogs())
+app.get('/blogs', (req, res, next) => {
+  req.data = dbApi.getBlogs()
+  next()
+})
+
+//增加一篇博客(传入一个博客对象)
+app.post("/addBlog", (req, res, next) => {
+  const blog = req.body
+  req.data = dbApi.addBlog(blog)
+  next()
+})
+
+//根据博客id删除博客
+app.delete("/deleteBlog/:id", (req, res, next) => {
+  req.data = dbApi.deleteBlog(req.params.id)
+  next()
 })
 
 
+//修改博客
+app.patch("/patchBlog",(req,res,next)=>{
+  req.data = dbApi.patchBlog(req.body)
+  next()
+})
 
 
+//通过标签名查找博客id---暂不支持
 
-//如果找不到对应的接口
-app.get('/*', (req, res,next) => {
-  res.send("404 not found")
+
+//------标签相关------------------
+//获取所有的标签
+app.get('/tags', (req, res, next) => {
+  req.data = dbApi.getTags()
+  next()
+})
+-
+//添加标签
+app.get('/tags', (req, res, next) => {
+  req.data = dbApi.addTag(req.body)
+  next()
+})
+
+//用户相关
+//注册用户
+app.post("/register", (req, res, next) => {
+  req.data = dbApi.register(req.body)
+  next()
+})
+
+//登录
+app.post("/login",(req, res, next) => {
+  req.data = dbApi.login(req.body)
+  next()
+})
+
+//最后处理的中间件
+app.get('/*', (req, res, next) => {
+  let result = {
+    success: true,
+    code: 200,
+    message: "ok",
+    data: {}
+  };
+  if (req.data === false) {
+    result.code = 400
+    result.success = false
+    result.message = "请求有误,未成功"
+  } else if (req.data) {
+    result.data = req.data
+  } else {
+    result.success = false
+    result.code = 404
+    result.message = "not found"
+  }
+  res.send(result)
 })
 
 app.listen(port, () => {
